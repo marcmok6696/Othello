03da301d62ef0dc4220bcdd9b79edee014614ea2
Marc Mok <34096460+marcmok6696@users.noreply.github.com>
2019-10-06 16:59:35 +0800

    Update Human.py

--- Human.py
+++ Human.py
@@ -1,18 +1,4 @@
-# CSCI3180 Principles of Programming Languages
-#
-# --- Declaration ---
-#
-# I declare that the assignment here submitted is original except for source
-# material explicitly acknowledged. I also acknowledge that I am aware of
-# University policy and regulations on honesty in academic work, and of the
-# disciplinary guidelines and procedures applicable to breaches of such policy
-# and regulations, as contained in the website
-# http://www.cuhk.edu.hk/policy/academichonesty/
-#
-# Assignment 2
-# Name : MOK KA SHUN
-# Student ID : 1155081387
-# Email Addr : marcmokkashun@gmail.com
+
 from Player import Player
 
 class Human(Player):
